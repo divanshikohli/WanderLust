7a470312ddde3ba0c67a8048568b6725de7363fe
Divanshi <divanshi2504@gmail.com>
2026-05-03 19:54:45 +0530

    Changes in index file

--- init/index.js
+++ init/index.js
@@ -1,11 +1,17 @@
+const path = require("path");
+require("dotenv").config({
+    path: path.resolve(__dirname, "../.env")
+});
+
+
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const dbUrl = process.env.ATLASDB_URL;
 
 async function main() {
-    await mongoose.connect(MONGO_URL);
+    await mongoose.connect(dbUrl);
     console.log("connected to DB");
     await initDB();
 }
@@ -15,7 +21,7 @@ main().catch((err) => {
 
 const initDB = async () => {
     await Listing.deleteMany({});
-    initData.data = initData.data.map((obj) => ({...obj, owner : "69f21e3a4a46ee595fbbc8e9"}));
+    initData.data = initData.data.map((obj) => ({ ...obj, owner: "69f21e3a4a46ee595fbbc8e9" }));
     await Listing.insertMany(initData.data);
     console.log("data was initialized");
 }
